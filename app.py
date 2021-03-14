66fb861a761e5158d501fa03329b7fb8ffef7fff
meltaylor78 <meltaylor78@gmail.com>
2021-03-14 15:01:09 +0000

    added logout function, added detail to the profile page with some css styling

--- app.py
+++ app.py
@@ -97,6 +97,13 @@ def add_record():
     return render_template("add_record.html", cars=cars, garages=garages)
 
 
+@app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
