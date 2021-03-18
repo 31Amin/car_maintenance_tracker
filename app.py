02a6180373aadb96321a6384c73dbe9cec30003c
meltaylor78 <meltaylor78@gmail.com>
2021-03-18 21:06:33 +0000

    updated admin mail link on userprofile page, improve contrast with background, added on hover to indicated it is activate

--- app.py
+++ app.py
@@ -55,13 +55,12 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        existing_user = mongo.db.directory.find_one(
+        user = mongo.db.directory.find_one(
             {"username": request.form.get("username").lower()})
-        name =  existing_user["name"]
 
-        if existing_user:
+        if user:
             if check_password_hash(
-            existing_user["password"], request.form.get("password")):
+                user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
                 return redirect(url_for("userprofile", username=session["user"]))
